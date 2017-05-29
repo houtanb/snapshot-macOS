c028272a9ba572da75e245eff97fa2b8b3fb78c2
Houtan Bastani <houtan@dynare.org>
2017-05-29 15:45:14 +0200

    fix scripts

--- createOsxFolderForPkg.sh
+++ createOsxFolderForPkg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
+
 TOP_DIR=$1
 TOP_DYN_DIR=$TOP_DIR/$2
-VERSION=$3
+INSTALLDIRNAME=$3
 
-INSTALLDIRNAME=dynare-${VERSION}-osx
 INSTALLDIR=$TOP_DIR/$INSTALLDIRNAME
 rm -rf $INSTALLDIR
 mkdir $INSTALLDIR
@@ -47,7 +47,7 @@ cp -r $TOP_DYN_DIR/examples                                      $INSTALLDIR
 # CONFIGURE #
 #############
 ./configure FFLAGS='-mmacosx-version-min=10.8' CPPFLAGS='-mmacosx-version-min=10.8' LDFLAGS='-mmacosx-version-min=10.8' \
-            --with-matlab=/Volumes/Storage/MATLAB/MATLAB_R2009b.app MATLAB_VERSION=7.9 MEXEXT=mexmaci64 --disable-octave \
+            --with-matlab=/Applications/MATLAB_R2012a.app MATLAB_VERSION=R2012a MEXEXT=mexmaci64 --disable-octave \
             --with-matio=/usr/local/static --with-gsl=/usr/local/static --with-slicot=/usr/local/static
 make clean
 
--- make-package.sh
+++ make-package.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 set -ex
 
+DATE=`date +%Y-%m-%d`
 TOP_DIR="/Users/houtanb/Documents/DYNARE/package"
 PKG_DIR="$TOP_DIR/macosx"
 WORK_DIR="$TOP_DIR/scratch"
 LAST_SHA_FILENAME="$TOP_DIR/last_sha.txt"
+
+if [ ! -d $WORK_DIR ]; then
+    mkdir $WORK_DIR
+fi
+
+if [ ! -f $LAST_SHA_FILENAME ]; then
+    touch $LAST_SHA_FILENAME
+fi
 
 cd $WORK_DIR
 rm -rf dynare-*
 wget http://www.dynare.org/snapshot/source/dynare-latest-src.tar.xz
 tar zxvf dynare-latest-src.tar.xz
 
-DIR="$(basename dynare-master*)"
-DATE=`echo $DIR | cut -d'-' -f3,4,5`
-SHA=`echo $DIR | cut -d'-' -f6`
+DIR="$(basename dynare-4*)"
+VER=`echo $DIR | cut -d'-' -f2,3`
+SHA=`echo $DIR | cut -d'-' -f4`
 LAST_SHA=`cat $LAST_SHA_FILENAME`
+
 if [ "$SHA" == "$LAST_SHA" ]
 then
     exit 0
 fi
 
-$TOP_DIR/createOsxFolderForPkg.sh $WORK_DIR $DIR $DATE
+SHORTSHA=`echo $SHA | cut -c1-7`
+FINALBASENAME=dynare-$VER-$DATE-$SHORTSHA-osx
+
+$TOP_DIR/createOsxFolderForPkg.sh $WORK_DIR $DIR $FINALBASENAME
 
 echo $SHA > $LAST_SHA_FILENAME
 
 cd $WORK_DIR
-zip -r dynare-$DATE-$SHA-osx.zip dynare-$DATE-osx
+zip -r $FINALBASENAME.zip $FINALBASENAME
 
 cd $TOP_DIR
 rm -f $PKG_DIR/dynare-latest-osx.zip
-cp -f $WORK_DIR/dynare-$DATE-$SHA-osx.zip $PKG_DIR/dynare-latest-osx.zip
-mv -f $WORK_DIR/dynare-$DATE-$SHA-osx.zip $PKG_DIR
+cp -f $WORK_DIR/$FINALBASENAME.zip $PKG_DIR/dynare-latest-osx.zip
+mv -f $WORK_DIR/$FINALBASENAME.zip $PKG_DIR
 rm -rf $WORK_DIR/dynare-*
 find $PKG_DIR -type f | sort -r | awk 'NR>11' | xargs rm
 set +e
 `date +%Y-%m-%d`
 rsync -v -r -t -e 'ssh -i /Users/houtanb/Documents/DYNARE/package/snapshot-manager_rsa' --delete $PKG_DIR snapshot-manager@kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/snapshot
-
+echo "Done building $FINALBASENAME.zip"
